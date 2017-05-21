9bae3ac8ec6855578c756b25715e707e038f6bf3
Raj Singh <raj@foreversingh.com>
2017-05-21 18:12:15 -0500

    Basic app is working. Slight styling.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
+  render() {
+    return (
+      <div className='search-bar'>
+        <input
+          placeholder='Search YouTube'
+          value={ this.state.term }
+          onChange={ e => this.onInputChange(e.target.value) } />
+      </div>
+    )
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+}
 
 export default SearchBar;
